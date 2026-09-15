978b0b0872b6ffa19015718ba7ade009a09e119e
AmirAli <amir.ali.its.me@gmail.com>
2026-09-15 11:05:12 +0330

    separating UI from logic

--- library.py
+++ library.py
@@ -47,100 +47,22 @@ class Library:
             self.next_member_id += 1
             return int("2020" + str(self.next_member_id))
 
-    def wait_for_continue(self):
-        input(f"\n{8*" "}Press Enter to return to menu.")
-
-    def add_book(self):
-        while True:
-            title = input(f"{8*" "}title : ")
-            author = input(f"{8*" "}author : ")
-            year = input(f"{8*" "}year : ")
-
-            approval = input(f"{8*" "}{title} by {author} in ({year}). \
-do you want to save this? (y/n)").lower()
-
-            if approval in ["y", "yes"]:
+    def add_book(self, title, author, year):
                 book_id = self.id_generate("book")
                 new_book = Book(title, author, year, book_id)
                 self.books.append(new_book.to_dict())
-                print(f"{8*" "}Book added successfully.")
 
-                self.wait_for_continue()
-
-                break
-            elif approval in ["n", "no"]:
-                print(f"{8*" "}Enter boobk information again. ")
-            else:
-                print(f"{8*" "}Please enter y/yes or n/no.")
-            
-    def add_member(self):
-        name = input(f"{8*" "}your name : ")
-        while True:
-            phone_number = input(f"{8*" "}phone number(+989123456789) : ")
-            if phone_number.startswith("+989") \
-                and len(phone_number) == 13 \
-                and phone_number[4:].isdigit():
-                break
-            else:
-                print(f"{8*" "}please enter your number in (+989123456789) format. ")
-
-        while True:
-            password1 = input(f"{8*" "}password : ")
-            password2 = input(f"{8*" "}re-enter password : ")
-            if password1 == password2:
-                password = password1
-                member_id = self.id_generate("member")
-                break
-            else:
-                print(f"{8*" "}password does not match. try again.")
-
+    def add_member(self, name, password, phone_number):
+        member_id = self.id_generate("member")
         new_member = Member(member_id, name, password, phone_number)
         self.members.append(new_member.to_dict())
 
-        print(
-            f"\n\tmember {name}, {phone_number} created successfully."
-            f"\nyour id is {member_id}, please remember your id."
-            )
-
-        self.wait_for_continue()
-
-    def show_data(self, type_of_data):
-        option_num = 1
+    def get_data(self, type_of_data):
         if type_of_data == "books":
-            if not self.books:
-                print(f"{8*" "}no book found.")
-            else:
-                for book in self.books:
-                    print(f"{8*" "}{"-" * 15}")
-                    print(f"{6*" "}{option_num}.")
-                    option_num += 1
-
-                    print(
-                        f"{8*" "}Title : {book["title"]}\n"
-                        f"{8*" "}Aothur : {book["author"]}\n"
-                        f"{8*" "}Year : {book["year"]}\n"
-                        f"{8*" "}Is Borrowed : {"NO" if book["is_borrowed"] else "Yes"}"
-                        )
+            return self.books
 
         if type_of_data == "members":
-            if not self.members:
-                print(f"{8*" "}no member found.")
-            else:
-                for member in self.members:
-                    print(f"{8*" "}{"-" * 15}")
-                    print(f"{6*" "}{option_num}.")
-                    option_num += 1
-
-                    print(
-                        f"{8*" "}Member ID : {member["member_id"]}\n"
-                        f"{8*" "}Name : {member["name"]}\n"
-                        f"{8*" "}Phone Number : {member["phone_number"]}\n"
-                        f"{8*" "}Membership Date : {member["membership_date"]}\n"
-                    )
-
-        print(f"{8*" "}{"-" * 15}")
-
-        self.wait_for_continue()
+             return self.members
 
     def search_books(self, category, search_term):
         search_term = search_term.lower()
@@ -161,5 +83,3 @@ do you want to save this? (y/n)").lower()
                         search_result.append(book)
 
         return search_result
-
-
--- main.py
+++ main.py
@@ -2,47 +2,120 @@ from library import Library
 
 library_start = Library()
 
+def wait_for_continue():
+    input(f"\n{8*" "}Press Enter to return to menu.")
+
 while True:
     print("""
-\t------------------------------
-\t****** library management *****
-\t------------------------------
+f"{8*" "}------------------------------
+f"{8*" "}****** library management *****
+f"{8*" "}------------------------------
     
-\t1. Add Book
-\t2. Add Member
-\t3. Show Books
-\t4. Show Members
-\t5. Search Book
-\t99. Exit
+f"{8*" "}1. Add Book
+f"{8*" "}2. Add Member
+f"{8*" "}3. Show Books
+f"{8*" "}4. Show Members
+f"{8*" "}5. Search Book
+f"{8*" "}99. Exit
 """)
     while True:
-        user_input = input("\tSelect a number : ")
+        user_input = input(f"{8*" "}Select a number : ")
 	    
         if user_input.isdigit():
             user_input = int(user_input)
             break
         else:
-            print("\tPlease enter a number.")
+            print(f"{8*" "}Please enter a number.")
 
-    if user_input == 1:
-        print("\n\tPlease enter book informations.")
-        library_start.add_book()
+    if user_input == 1: #Add Book
+        print(f"\n{8*" "}Please enter book informations.")
 
-    elif user_input == 2:
-        print("\n\tPlease enter member informations.")
-        library_start.add_member()
+        while True:
+            title = input(f"{8*" "}title : ")
+            author = input(f"{8*" "}author : ")
+            year = input(f"{8*" "}year : ")
+        
+            approval = input(f"{8*" "}{title} by {author} in ({year}). \
+do you want to save this? (y/n)").lower()
+            if approval in ["y", "yes"]:
+                library_start.add_book(title, author, year)
+                print(f"{8*" "}Book added successfully.")
+                wait_for_continue()
 
-    elif user_input == 3:
-        print("\n\tThe list of library's Books : \n")
-        library_start.show_data("books")
+                break
 
-    elif user_input == 4:
-        print("\n\tThe list of Members : \n")
-        library_start.show_data("members")
+            elif approval in ["n", "no"]:
+                print(f"{8*" "}Enter boobk information again. ")
 
-    elif user_input == 5:
+            else:
+                print(f"{8*" "}Please enter y/yes or n/no.")
+
+    elif user_input == 2: #Add Member
+        print(f"\n{8*" "}Please enter member informations.")
+        name = input(f"{8*" "}your name : ")
+        while True:
+            phone_number = input(f"{8*" "}phone number(+989123456789) : ")
+            if phone_number.startswith("+989") \
+                and len(phone_number) == 13 \
+                and phone_number[4:].isdigit():
+                break
+            else:
+                print(f"{8*" "}please enter your number in (+989123456789) format. ")
+
+        while True:
+            password1 = input(f"{8*" "}password : ")
+            password2 = input(f"{8*" "}re-enter password : ")
+            if password1 == password2:
+                password = password1
+                break
+            else:
+                print(f"{8*" "}password does not match. try again.")
+                
+        library_start.add_member(name, password, phone_number)
+        print(f"\n\tmember {name}, {phone_number} created successfully.")
+
+    elif user_input == 3: #Show Books
+        print(f"\n{8*" "}The list of library's Books : \n")
+        books = library_start.get_data("books")
+
+        if not books:
+            print(f"{8*" "}no book found.")
+        else:
+            for option_num, book in enumerate(books, start=1):
+                print(f"{8*" "}{"-" * 15}"
+                      f"{6*" "}{option_num}.")
+
+                print(
+                    f"{8*" "}Title : {book["title"]}\n"
+                    f"{8*" "}Aothur : {book["author"]}\n"
+                    f"{8*" "}Year : {book["year"]}\n"
+                    f"{8*" "}Is Borrowed : {"Yes" if book["is_borrowed"] else "No"}"
+                )
+                print(f"{8*" "}{"-" * 15}")
+
+    elif user_input == 4: #Show Members
+        print(f"\n{8*" "}The list of Members : \n")
+        members = library_start.get_data("members")
+
+        if not members:
+            print(f"{8*" "}no member found.")
+        else:
+            for option_num, member in enumerate(members, start=1):
+                print(f"{8*" "}{"-" * 15}"
+                      f"{6*" "}{option_num}.")
+
+                print(
+                    f"{8*" "}Member ID : {member["member_id"]}\n"
+                    f"{8*" "}Name : {member["name"]}\n"
+                    f"{8*" "}Phone Number : {member["phone_number"]}\n"
+                    f"{8*" "}Membership Date : {member["membership_date"]}\n"
+                )
+                print(f"{8*" "}{"-" * 15}")
+
+
+    elif user_input == 5: #Search Book
         pass
 
-    elif user_input == 99:
+    elif user_input == 99: #Exit
         break
 
